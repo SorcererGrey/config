c83e2f078e6d1ea0a157eb92f29d7a6b4a807f27
Dylan Lavery <laverydylan@gmail.com>
2017-05-15 20:38:12 +0100

    Rename class file and add comments to the class

--- cmanager.v1a.php
+++ cmanager.v1a.php
@@ -2,20 +2,23 @@
 
 /**
  * Class: Configuration manager
- * Version: 1
+ * Version: 1a
  */
 
 class ConfigManager {
 
+    // Variables
     var $defFileExt = '.conf.json';
     var $givenFilePath;
     var $defFileContent = array();
     var $debug = false;
 
+    // Class constructor
     function __construct ($filePath) {
 
         $this -> givenFilePath = $filePath;
 
+        // If debug is enabled ignore file existance check and delete existing file if present
         if ($this -> debug) {
 
             if (file_exists($filePath . $this -> defFileExt)) {
@@ -30,6 +33,7 @@ class ConfigManager {
 
         } else {
 
+            // Check if file doesnt exist before creating new one
             if (!file_exists($filePath . $this -> defFileExt)) {
 
                 $configFile = fopen($this -> givenFilePath . $this -> defFileExt, 'a');
@@ -42,6 +46,14 @@ class ConfigManager {
 
     }
 
+    /**
+     * Get the value associated with a provided key
+     * @param Int  $depth The depth of the stored value within the config file
+     * @param String  $key The key to identify the value
+     * @param String $secondKey The second key to identify the value
+     * @param String $thirdKey The third key to identify the value
+     * @return String The value associated with the key
+     */
     function Get ($depth, $key, $secondKey = 0, $thirdKey = 0) {
 
         $decodedConfig = json_decode(file_get_contents($this -> givenFilePath . $this -> defFileExt), true);
@@ -69,14 +81,23 @@ class ConfigManager {
 
     }
 
+    /**
+     * Set a new value or change an existing value in a config file
+     * @param String $key The key to associate with the value
+     * @param String $value The value to be associated with the key
+     */
     function Set ($key, $value) {
 
+        // Decode json from the file specified
         $decodedConfig = json_decode(file_get_contents($this -> givenFilePath . $this -> defFileExt), true);
 
+        // If the given value isn't empty
         if ($value !== 0) {
 
+            // Create new key:value entry
             $decodedConfig[$key] = $value;
 
+            // Write the the new config back to the specified file
             $configFile = fopen($this -> givenFilePath . $this -> defFileExt, 'w');
             fwrite($configFile, json_encode($decodedConfig, JSON_PRETTY_PRINT));
             fclose($configFile);
